94f4c40356f213358e7e925928231408499c9de5
ashurbz <rbzashu1@gmail.com>
2020-09-03 13:11:46 +0530

    completed

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,3 @@
-// getting no. of habits in list 
-//if no habit found then display a  error notification 
-if(document.getElementsByTagName("li").length<=1)
-     {
-        $.notify("Add Some Habits","success");
-    
-     }
 
 
 // adding a onclick listener to status toggle button
@@ -12,7 +5,6 @@ function change(e){
      console.log( e.children[0].value);
 //if class is ban then do it check
     if(e.children[0].value==" "){
-        $.notify("Done!",{ autoHideDelay: 500});
       e.children[0].value="done";
     
     e.className="fa fa-check";
@@ -24,7 +16,7 @@ function change(e){
 //if class is check then do it times
 
    else if(e.children[0].value=="done"){
-    $.notify("Not Done!",{ autoHideDelay: 500});
+
    e.children[0].value="notDone";
     
     e.className="fa fa-times";
@@ -34,7 +26,6 @@ function change(e){
 
 //   if class is times then do it ban
   else  if(e.children[0].value=="notDone"){
-    $.notify("No Action",{ autoHideDelay: 500});
    e.children[0].value=" ";
     e.className="fa fa-ban";
     console.log( e.children[0].value);
